0dce50f8bc68d68458d67910f1a2e5b16e54ee0f
Christian Stöger <christian.stoeger@live.de>
2023-12-20 08:27:43 +0100

    added keymappings for cmake tasks

--- lua/plugins/neovim-tasks.lua
+++ lua/plugins/neovim-tasks.lua
@@ -1,6 +1,17 @@
 return {
     "Shatur/neovim-tasks",
-    opts = {
-        params_file = "cmake.json",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
     },
+    config = function()
+        local tasks = require("tasks")
+        tasks.setup({
+            params_file = "cmake.json",
+        })
+
+        -- set keymaps
+        vim.keymap.set("n", "<leader>cmc", "<cmd>Task start cmake configure<cr>", { desc = "Task: CMake configure" })
+        vim.keymap.set("n", "<leader>cmb", "<cmd>Task start cmake build<cr>", { desc = "Task: CMake build" })
+        vim.keymap.set("n", "<leader>cmx", "<cmd>Task start cmake clean<cr>", { desc = "Task: CMake clean" })
+    end,
 }
